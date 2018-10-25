edcea24a7fecb50f9ae46f5a1782d1ad8924bd3d
fernt877 <fernt877@newschool.edu>
2018-10-25 19:45:23 -0400

    Add files via upload

---
+++ tool_concept/scripts.js
@@ -0,0 +1,24 @@
+var verbs, nouns, adjectives, adverbs, preposition;
+nouns = ["horror", "crows", "Transylvania", "eyeballs", "monsters", "superstition", "tales of horror", "werewolves", "vampires", "a graveyard", "a prankster", "tricks", "a tombstone", "the undead", "a broomstick", "the headless horseman", "a flashlight", "ghost stories", "fangs", "candy", "spirits", "spider webs", "nightfall", "full moon", "pumpkin patch",];
+verbs = ["screaming", "surprised", "creeped", "panicing", "spooked", "raising hell", "trick-or-treating", "party", "playing pranks", "vexing", "at risk", "hooting", "petrified", "distressed", "afraid",];
+adjectives = ["abominable", "blood-curdling", "supernatural", "eerie", "fear-inspiring", "wicked", "ghoulish", "up-to-no-good", "bone-chilling", "bloody", "chilling", "cowardly", "disguised", "dire", "deadly", "dark", "corpselike", "",];
+adverbs = ["slowly", "elegantly", "precisely", "quickly", "sadly", "humbly", "proudly", "shockingly", "calmly", "passionately"];
+preposition = ["down", "into", "up", "on", "upon", "below", "above", "through", "across", "towards"];
+
+            function randGen() {
+              return Math.floor(Math.random() * 5);
+            }
+
+            function sentence() {
+              var rand1 = Math.floor(Math.random() * 10);
+              var rand2 = Math.floor(Math.random() * 10);
+              var rand3 = Math.floor(Math.random() * 10);
+              var rand4 = Math.floor(Math.random() * 10);
+              var rand5 = Math.floor(Math.random() * 10);
+              var rand6 = Math.floor(Math.random() * 10);
+              //                var randCol = [rand1,rand2,rand3,rand4,rand5];
+              //                var i = randGen();
+              var content = "No" + " longer " + verbs[rand1] + " " + " this " + " Halloween " + ", " + " " + " the " + " room " + " would " + " still " + " be " + " haunted " + " by " + adjectives[rand3] + " " + " memories " + " and " + nouns[rand4] + " " + ".";
+              document.getElementById('sentence').innerHTML = "&quot;" + content + "&quot;";
+            };
+            sentence();
